f94740d56302e4a23742f0d35654a177a5904b9d
ZoeLi <zoeli@tstartel.com>
2020-05-23 05:07:42 +0800

    createProject

--- Slider/ViewController.swift
+++ Slider/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var girlImageView: UIImageView!
+    
+    @IBOutlet weak var balloonImageView: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    
+    
+    
+    @IBAction func didTapSlider(_ sender: UISlider) {
+        
+        
+        let speed = sender.value * 65
+        
+        let size = sender.value * 70
+        
+        self.balloonImageView.frame.origin.x = CGFloat(378-speed * 6)
+        
+        self.balloonImageView.frame.size = CGSize(width: CGFloat(201 - size), height: CGFloat(249 - size))
+        
+       
+        if self.balloonImageView.frame.origin.x == 378 {
+           
+            self.girlImageView.image = UIImage(named: "girl_happy" )
+        
+        }else if self.balloonImageView.frame.origin.x < 378 && self.balloonImageView.frame.origin.x > 164 {
+            
+            self.girlImageView.image = UIImage(named: "girl_notHappy" )
+            
+        }else {
+            
+            self.girlImageView.image = UIImage(named: "girl_cry" )
+            
+        }
 
-
+    }
+    
+    
+    
+    
+    
+    
 }
-
